5e8a4bd37111d7b65128d1daea4159de24569193
osiel <osiel@kuidamos.com>
2025-05-16 08:17:39 -0300

    Estudando conversão de tipos no kotlin

---
+++ src/main/kotlin/conversaodetipos/Main.kt
@@ -0,0 +1,12 @@
+package conversaodetipos
+
+fun main() {
+    val str = "true"
+    println(str.toBoolean())
+
+    val a: Byte = 20
+    val b: Int = 50
+
+    println(a.toInt())
+    println(b.toString())
+}
